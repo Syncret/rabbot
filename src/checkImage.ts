38bb127394aa2eac1c3790082772a08b7a20fcc8
Syncret <syncret@outlook.com>
2020-12-16 21:20:06 +0800

    update message

--- src/checkImage.ts
+++ src/checkImage.ts
@@ -34,10 +34,18 @@ export function apply(ctx: Context, options: Options) {
           let resMsg = "";
           if (tags.porn > 83) {
             resMsg = "太瑟情啦！赶紧撤回！";
+          } else if (tags.porn > 50) {
+            resMsg = "感觉好黄暴！";
+          } else if (tags.hot > 80) {
+            resMsg = "好色哦！";
           } else if (tags.hot > tags.normal) {
             resMsg = "好像是一张色图的样子！";
+          } else if (tags.normal > 90) {
+            resMsg = "完全safe呢！";
+          } else if (tags.normal > 70) {
+            resMsg = "看起来还蛮健全的嘛...";
           } else {
-            resMsg = "看起来还蛮健全的呢...";
+            resMsg = "有点微妙的感觉...";
           }
           resMsg = resMsg + ` 综合瑟琴指数: ${tags.normal_hot_porn}`;
 
