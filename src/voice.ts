6190e8ee7bd4647b5b11cfef0d595474b426e0e4
Syncret <syncret@outlook.com>
2020-12-18 20:58:31 +0800

    add group option for voice

--- src/voice.ts
+++ src/voice.ts
@@ -1,25 +1,21 @@
 import { Context } from "koishi-core";
-import {
-  tencentAIApis,
-} from "./TencentAiApis";
+import { tencentAIApis } from "./TencentAiApis";
 
 export interface Options {}
 
 export function apply(ctx: Context) {
-  ctx.addMiddleware((meta, next) => {
-    return next();
-  });
   ctx
     .command("voice <message...>", "say message in voice")
     .option("-p, --speaker <speaker>", "specify the speaker, 1/5/6/7")
     .option("-s, --speed <speed>", "specify the speed, [50, 200]")
     .option("-v, --volumn <volumn>", "specify the volumn, [-10, 10]")
     .option("-a, --aht <aht>", "specify the aht, [-24, 24]")
+    .option("-g, --group <group>", "send the voice to group")
     .action(({ meta, options = {} }, message) => {
       message = message.trim();
       if (message) {
-        if(message.length>30){
-          message="太长了！不想念";
+        if (message.length > 30) {
+          message = "太长了！不想念";
         }
         tencentAIApis
           .aaiTts(message, {
@@ -30,7 +26,11 @@ export function apply(ctx: Context) {
           })
           .then((data) => {
             const res = `[CQ:record,file=base64://${data}]`;
-            meta.$send!(res);
+            if (options.group) {
+              ctx.sender.sendGroupMsgAsync(options.group, res);
+            } else {
+              meta.$send!(res);
+            }
           })
           .catch((e) => {
             meta.$send!(e.message);
