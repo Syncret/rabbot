dce82d1006b1908ff4eee7ab739d18604f5f5415
Syncret <syncret@outlook.com>
2021-03-07 22:29:46 +0800

    add plugin names

--- src/translator.ts
+++ src/translator.ts
@@ -167,3 +167,4 @@ export function apply(ctx: Context) {
       }
     });
 }
+export const name = "TencentTranslator";
--- src/voice.ts
+++ src/voice.ts
@@ -94,3 +94,4 @@ export function apply(ctx: Context, options: Options = {}) {
       }
     });
 }
+export const name = "TencentAIVoice";
