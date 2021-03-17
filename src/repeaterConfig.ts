6c458e97d3f90d7d14750bbb6ae1407024b2cc13
Yusheng Jiang <yujian@microsoft.com>
2021-03-17 18:17:30 +0800

    add repeat checker

--- src/repeaterConfig.ts
+++ src/repeaterConfig.ts
@@ -21,14 +21,11 @@ export const repeaterConfig: Config = {
     if (state.times > 2 && !state.repeated && Random.bool(0.4)) {
       return state.content;
     }
-    if (state.times >= 4 && Random.bool(0.3)) {
-      const banUser = pickRandomly(Object.keys(state.users));
-      ban(session, banUser, state.times * 60);
+    if (state.times >= 2 && Random.bool(state.times / 20)) {
+      ban(session, session.userId!, state.times);
       return (
-        `复读机失控了！兔兔不得不随机逮捕一个复读机禁言。` +
-        `那个人就是...${segment.at(banUser)}! 禁言时间为复读次数${
-          state.times
-        }分钟!`
+        `复读机失控了！兔兔只来得及逮捕最后一个复读机！` +
+        `那个人就是...${segment.at(session.userId!)}! `
       );
     }
   },
@@ -39,8 +36,20 @@ const rolePermissionMap = {
   admin: 2,
   member: 1,
 };
+let banRecord: Record<string, number> = {};
+let banDate = -1;
+function addTodayBanRecord(userId: string) {
+  let today = new Date().getDate();
+  if (today !== banDate) {
+    banDate = today;
+    banRecord = {};
+  }
+  let banTimes = banRecord[userId] || 0;
+  banRecord[userId] = ++banTimes;
+  return banTimes;
+}
 
-async function ban(session: Session, userId:string,  time: number): Promise<void> {
+async function ban(session: Session, userId: string, time: number): Promise<void> {
   const { groupId, selfId } = session;
   if (!groupId || !selfId || !userId) {
     throw Error("Invalid parameters.");
@@ -49,17 +58,27 @@ async function ban(session: Session, userId:string,  time: number): Promise<void
   if (self.role !== "admin" && self.role !== "owner") {
     return;
   }
+  const banCount = addTodayBanRecord(userId);
+  const factor = 2 ** (banCount - 1);
+  const banTimeString = `${time}${banCount > 1 ? "x2".repeat(banCount - 1) + `=${time * factor}` : ""}分钟`;
   const user = await session.bot.$getGroupMemberInfo(groupId, userId);
   if (rolePermissionMap[self.role!] > rolePermissionMap[user.role!]) {
-    session.bot.$setGroupBanAsync(groupId, userId, time);
+    setTimeout(
+      () => {
+        session.send(`今天累计第${banCount}次被捕，${banCount > 1 ? "惩罚翻倍！" : ""}禁言时间为本次复读次数${banTimeString}!`);
+        session.bot.$setGroupBanAsync(groupId, userId, time * factor * 60);
+      },
+      500
+    );
+
   } else if (user.role === "admin") {
     setTimeout(
-      () => session.sendQueued("可恶，是狗管理，兔兔的力量还不够大..."),
+      () => session.sendQueued(`可恶，是狗管理，兔兔的力量还不够大...今天累计第${banCount}次被捕，请自觉禁言${banTimeString}。`),
       2000
     );
   } else if (user.role === "owner") {
     setTimeout(
-      () =>  session.sendQueued("啊，是群主，兔兔只是路过..."),
+      () => session.sendQueued("啊，是群主，兔兔只是路过..."),
       2000
     );
   }
