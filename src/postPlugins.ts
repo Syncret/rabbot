d4725f62f3bb3d1f7e37823e4426390d13957112
Syncret <syncret@outlook.com>
2021-03-21 23:34:49 +0800

    adjust command permission

--- src/postPlugins.ts
+++ src/postPlugins.ts
@@ -3,7 +3,7 @@ import { Context } from "koishi";
 export const PostPlugins = {
   name: "PostPlugins",
   apply: (ctx: Context) => {
-    ctx.command("shot", { authority: 3 });
-    ctx.command("recalled", { authority: 3 });
+    ctx.command("shot", { authority: 2 });
+    ctx.command("recalled", { authority: 2 });
   },
 };
