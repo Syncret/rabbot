44c3306090969255581b921b43da3ab6d8a29b29
syncret <syncret@outlook.com>
2023-02-27 04:06:06 +0000

    update chatgpt to 4.7

--- src/chatgpt-ts.ts
+++ src/chatgpt-ts.ts
@@ -3,24 +3,31 @@ import { Context } from "koishi-core";
 
 export interface Options {
   apiKey: string;
+  accessToken: string;
 }
 
 export const name = "ChatGPT";
 export async function apply(ctx: Context, options: Options) {
-  const { apiKey } = options;
+  const { accessToken } = options;
   const chatgpt = await import('chatgpt');
-  const api = new chatgpt.ChatGPTAPI({
-    apiKey
+  const api = new chatgpt.ChatGPTUnofficialProxyAPI({
+    accessToken,
+    apiReverseProxyUrl: "https://chat.duti.tech/api/conversation"
   });
   const defaultMessageOptions: SendMessageOptions = {
     timeoutMs: 2 * 60 * 1000
   };
+  let enabled = true;
+  let queueAllMessages = true;
+  let currentJobPromise: Promise<string> | undefined;
   const channelMessageCache: Record<string, ChatMessage> = {};
   const channelMessageStatus: Record<string, boolean> = {};
   ctx
     .command("chat", "Talk with chatgpt")
     .option("new", "-n Create a new session")
     .option("channelId", "-c Get channel Id", { hidden: true })
+    .option("on", "-o Turn on/off chat", { hidden: true, authority: 3 })
+    .option("queue", "-q Queue messages of all channels", { hidden: true, authority: 3 })
     .action(async ({ session, options = {} }, message) => {
       const channelId = session?.channelId;
       if (!channelId) {
@@ -29,8 +36,24 @@ export async function apply(ctx: Context, options: Options) {
       if (options.channelId) {
         return channelId;
       }
+      if (options.on) {
+        enabled = !enabled;
+        return `聊天功能已${enabled ? "开启" : "关闭"}`;
+      }
+      if (options.queue) {
+        queueAllMessages = !queueAllMessages;
+        return `消息队列已${queueAllMessages ? "开启" : "关闭"}`;
+      }
+      if (!enabled) {
+        return "兔兔的零花钱烧完了，聊天功能暂停中...";
+      }
       if (!message) {
-        return "你什么都没说呢";
+        if (options.new) {
+          delete channelMessageCache[channelId];
+          return "对话历史已清理";
+        } else {
+          return "你什么都没说呢";
+        }
       }
       const previousMessage = channelMessageCache[channelId];
       const chatStatus = channelMessageStatus[channelId];
@@ -39,13 +62,29 @@ export async function apply(ctx: Context, options: Options) {
       }
       try {
         channelMessageStatus[channelId] = true;
-        const res = await api.sendMessage(message, previousMessage && !options.new ? {
-          ...defaultMessageOptions,
-          conversationId: previousMessage.conversationId,
-          parentMessageId: previousMessage.id
-        } : defaultMessageOptions);
-        channelMessageCache[channelId] = res;
-        return res.text;
+        if (queueAllMessages && currentJobPromise != null) {
+          let lastAwaitJob: Promise<string> | undefined = undefined;
+          while (lastAwaitJob !== currentJobPromise) {
+            lastAwaitJob = currentJobPromise;
+            await currentJobPromise;
+            if (lastAwaitJob !== currentJobPromise) {
+              console.log(`${channelId} waits again`);
+            }
+          }
+        }
+        const sendMessage = async () => {
+          const res = await api.sendMessage(message, previousMessage && !options.new ? {
+            ...defaultMessageOptions,
+            conversationId: previousMessage.conversationId,
+            parentMessageId: previousMessage.id
+          } : defaultMessageOptions);
+          channelMessageCache[channelId] = res;
+          return res.text ? res.text : JSON.stringify(res);
+        };
+        const messagePromise = sendMessage();
+        currentJobPromise = messagePromise;
+        const result = await messagePromise;
+        return result;
       } catch (e) {
         return e + "";
       } finally {
--- src/chatgpt.js
+++ src/chatgpt.js
@@ -1,19 +1,25 @@
 const name = "ChatGPT";
 async function apply(ctx, options) {
-  const { apiKey } = options;
+  const { accessToken } = options;
   const chatgpt = await import('chatgpt');
-  const api = new chatgpt.ChatGPTAPI({
-    apiKey
+  const api = new chatgpt.ChatGPTUnofficialProxyAPI({
+    accessToken,
+    apiReverseProxyUrl: "https://chat.duti.tech/api/conversation"
   });
   const defaultMessageOptions = {
     timeoutMs: 2 * 60 * 1000
   };
+  let enabled = true;
+  let queueAllMessages = true;
+  let currentJobPromise;
   const channelMessageCache = {};
   const channelMessageStatus = {};
   ctx
     .command("chat", "Talk with chatgpt")
     .option("new", "-n Create a new session")
     .option("channelId", "-c Get channel Id", { hidden: true })
+    .option("on", "-o Turn on/off chat", { hidden: true, authority: 3 })
+    .option("queue", "-q Queue messages of all channels", { hidden: true, authority: 3 })
     .action(async ({ session, options = {} }, message) => {
       const channelId = session?.channelId;
       if (!channelId) {
@@ -22,8 +28,24 @@ async function apply(ctx, options) {
       if (options.channelId) {
         return channelId;
       }
+      if (options.on) {
+        enabled = !enabled;
+        return `聊天功能已${enabled ? "开启" : "关闭"}`;
+      }
+      if (options.queue) {
+        queueAllMessages = !queueAllMessages;
+        return `消息队列已${queueAllMessages ? "开启" : "关闭"}`;
+      }
+      if (!enabled) {
+        return "兔兔的零花钱烧完了，聊天功能暂停中...";
+      }
       if (!message) {
-        return "你什么都没说呢";
+        if (options.new) {
+          delete channelMessageCache[channelId];
+          return "对话历史已清理";
+        } else {
+          return "你什么都没说呢";
+        }
       }
       const previousMessage = channelMessageCache[channelId];
       const chatStatus = channelMessageStatus[channelId];
@@ -32,13 +54,29 @@ async function apply(ctx, options) {
       }
       try {
         channelMessageStatus[channelId] = true;
-        const res = await api.sendMessage(message, previousMessage && !options.new ? {
-          ...defaultMessageOptions,
-          conversationId: previousMessage.conversationId,
-          parentMessageId: previousMessage.id
-        } : defaultMessageOptions);
-        channelMessageCache[channelId] = res;
-        return res.text;
+        if (queueAllMessages && currentJobPromise != null) {
+          let lastAwaitJob = undefined;
+          while (lastAwaitJob !== currentJobPromise) {
+            lastAwaitJob = currentJobPromise;
+            await currentJobPromise;
+            if (lastAwaitJob !== currentJobPromise) {
+              console.log(`${channelId} waits again`);
+            }
+          }
+        }
+        const sendMessage = async () => {
+          const res = await api.sendMessage(message, previousMessage && !options.new ? {
+            ...defaultMessageOptions,
+            conversationId: previousMessage.conversationId,
+            parentMessageId: previousMessage.id
+          } : defaultMessageOptions);
+          channelMessageCache[channelId] = res;
+          return res.text ? res.text : JSON.stringify(res);
+        };
+        const messagePromise = sendMessage();
+        currentJobPromise = messagePromise;
+        const result = await messagePromise;
+        return result;
       } catch (e) {
         return e + "";
       } finally {
