fa28f659d7abd6690ef0eb38d3bd643ac62c07a1
syncret <syncret@outlook.com>
2023-04-04 08:08:11 +0000

    Add azure openai support for chatgpt

---
+++ src/azure-openai.ts
@@ -0,0 +1,110 @@
+import axios, { AxiosInstance } from "axios";
+import { Context } from "koishi-core";
+
+export interface Options {
+  resourceName: string;
+  accessKey: string;
+}
+
+export const name = "AzureOpenAI";
+const defaultSystemInstruction = "You're a chat bot named 兔兔, act and talk like a young 17 year old Japanese high school girl.";
+export async function apply(ctx: Context, options: Options) {
+  let { resourceName, accessKey } = options;
+  let enabled = true;
+  let queueAllMessages = true;
+  let currentJobPromise: Promise<string> | undefined;
+  let channelMessageCache: Record<string, string> = {};
+  let channelSystemCache: Record<string, string | undefined> = {};
+  const channelMessageStatus: Record<string, boolean> = {};
+  const axiosClient: AxiosInstance = axios.create({
+    baseURL: `https://${resourceName}.openai.azure.com/openai/deployments/gpt35/chat/completions`,
+    params: {
+      "api-version": "2023-03-15-preview",
+    },
+    headers: {
+      "api-key": accessKey,
+    },
+  });
+  ctx
+    .command("chat", "Talk with chatgpt")
+    .option("new", "-n Create a new session")
+    .option("clean", "-c Clean all conversations", { hidden: true, authority: 3 })
+    .option("on", "-o Turn on/off chat", { hidden: true, authority: 3 })
+    .option("queue", "-q Queue messages of all channels", { hidden: true, authority: 3 })
+    .option("proxy", "-p Switch api reverse proxy url", { hidden: true, authority: 3 })
+    .option("token", "-t Update access token", { hidden: true, authority: 3 })
+    .option("system", "-s Set system message", { hidden: true, authority: 3 })
+    .action(async ({ session, options = {} }, message) => {
+      const channelId = session?.channelId;
+      if (!channelId) {
+        return "Invalid channelId";
+      }
+      if (options.on) {
+        enabled = !enabled;
+        return `聊天功能已${enabled ? "开启" : "关闭"}`;
+      }
+      if (options.queue) {
+        queueAllMessages = !queueAllMessages;
+        return `消息队列已${queueAllMessages ? "开启" : "关闭"}`;
+      }
+      if (options.clean) {
+        channelMessageCache = {};
+        return "所有对话历史已清理";
+      }
+      if (options.new) {
+        delete channelMessageCache[channelId];
+        if (!message) {
+          return "对话历史已清理";
+        }
+      }
+      if (message?.length > 1500) { // check this before you use message in system or user
+        return "消息太长啦";
+      }
+      if (options.system) {
+        channelSystemCache[channelId] = message || "";
+      }
+      if (!enabled) {
+        return "兔兔的脑细胞烧完了，聊天功能暂停中...";
+      }
+      const chatStatus = channelMessageStatus[channelId];
+      if (chatStatus) {
+        return "还在回复上一条消息";
+      }
+      if (!message) {
+        return "你什么都没说呢";
+      }
+      channelMessageStatus[channelId] = true;
+      if (queueAllMessages && currentJobPromise != null) {
+        let lastAwaitJob: Promise<string> | undefined = undefined;
+        while (lastAwaitJob !== currentJobPromise) {
+          lastAwaitJob = currentJobPromise;
+          await currentJobPromise;
+          if (lastAwaitJob !== currentJobPromise) {
+            console.log(`${channelId} waits again`);
+          }
+        }
+      }
+      const sendMessage = async () => {
+        try {
+          const messages = [{ role: "user", content: message }];
+          const systemInstruction = channelSystemCache[channelId] == "" ? undefined : channelSystemCache[channelId] || defaultSystemInstruction;
+          if (systemInstruction) {
+            messages.unshift({ role: "system", content: systemInstruction });
+          }
+          const response = await axiosClient.post("", {
+            messages
+          })
+          const responseMessage = response.data?.choices?.[0]?.message;
+          const content = responseMessage?.content;
+          return content ?? responseMessage ?? response.data?.choices ?? response.data;
+        } catch (e) {
+          return e + "";
+        }
+      };
+      const messagePromise = sendMessage();
+      currentJobPromise = messagePromise;
+      const result = await messagePromise;
+      channelMessageStatus[channelId] = false;
+      return result;
+    });
+}
